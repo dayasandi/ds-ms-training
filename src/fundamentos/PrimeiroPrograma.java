60f318e959dca8b918589daae655f457f2d37ac7
Dayane <dayane.sandi@hotmail.com>
2023-04-02 14:25:41 -0300

    Virado no jiray - aula05

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,5 +1,21 @@
 package fundamentos;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PrimeiroPrograma {
+    public static void main(String[] args) {
+//        String nome = "Dayane";
+//        String sobreNome = "Sandi";
+//        System.out.println(nome +" "+ sobreNome);
+
+        List<String> nomes = Arrays.asList("Dayane", "Maria", "Jõao", "Pedro");
+        List<String> res = new ArrayList<>();
+
+        nomes.stream().filter(n -> n.contains("D") || n.contains("J"))
+                .forEach(n -> res.add(n));
+        System.out.println(res);
+    }
 
 }
